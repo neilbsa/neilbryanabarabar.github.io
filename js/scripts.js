00e0196fb3fd51e7df490880d137f61e1ee0aadb
Neil Bryan S Abarabar <nbsa@civicmdsg.com.ph>
2021-06-08 22:40:56 +0800

    setup  initial front page

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,47 @@
 // Scripts
 // 
 
+var stackMessages=[
+    "ASP.NET MVC Developer",
+    ".Net Core Developer",
+    "Nativescript Developer",
+    "Angular Developer",
+    "Typescript Developer",
+    "JQuery Developer",
+    "Clean Architecture Compliance",
+    "Docker Deployment",
+
+   
+    "Javascript Developer",
+    "Wordpress Developer",
+    "MSSQL Administrator",
+    "Microservice Compliance",
+    "WPF Developer",
+    "Bootstrap",
+    "Kubernetes",
+    "Python Developer"
+    ];
+var rank=0;
+
+// Code for Chrome, Safari and Opera
+document.getElementById("currentTypingWriter").addEventListener("webkitAnimationEnd", changeTxt);
+
+// Standard syntax
+document.getElementById("currentTypingWriter").addEventListener("animationend", changeTxt);
+
+function changeTxt(e){
+  _h1 = this.getElementsByTagName("h4")[0];
+  _h1.style.webkitAnimation = 'none'; // set element animation to none
+   setTimeout(function() { // you surely want a delay before the next message appears
+      _h1.innerHTML=stackMessages[rank];
+      var speed =1.5*stackMessages[rank].length/20; // adjust the speed (3.5 is the original speed, 20 is the original string length
+      _h1.style.webkitAnimation = 'typing '+speed+'s steps(40, end), blink-caret .75s step-end infinite'; //  switch to the original set of animation      
+      (rank===stackMessages.length-1)?rank=0:rank++; // if you have displayed the last message from the array, go back to the first one, else go to next message
+    }, 1000);
+}
+
+
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
